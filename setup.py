934aab830c7accaa57f6ebc71d1f729b41107c63
Ctesr <160390018+Ctesr@users.noreply.github.com>
2025-03-17 15:50:30 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 APP = ['remove_transparency.py']  # 替换为你的Python脚本文件名
 DATA_FILES = []
 OPTIONS = {
-    'argv_emulation': True,
+    'argv_emulation': False,
     'packages': ['PIL'],  # 明确指定 Pillow 的包名
     'includes': ['PIL'],  # 确保 PIL 被包含
     'iconfile': 'ico.ico'
